ba25bbf1e831e89733b9ff1b7a89002ff6526c9e
StanislavAshykhmin <ashykhminstanislav@gmail.com>
2017-12-18 20:51:10 +0200

    delete header and footer

---
+++ product_list.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$arr = array(
+array('Model' =>'iPhone 7 16GB', 'Price' => '99$');
+array('Model' =>'iPhone 7 32GB', 'Price' => '199$');
+array('Model' =>'iPhone 7 64GB', 'Price' => '299$');
+array('Model' =>'iPhone 7 Plus 16GB', 'Price' => '199$');
+array('Model' =>'iPhone 7 Plus 32GB', 'Price' => '299$');
+array('Model' =>'iPhone 7 Plus 64GB', 'Price' => '399$');
+);
+
+ ?>
