80cff8e6fc7393f3b9139e523755c0f312dd10a8
StanislavAshykhmin <ashykhminstanislav@gmail.com>
2017-12-18 20:50:41 +0200

    delete header and footer

---
+++ cart.php
@@ -0,0 +1,105 @@
+<?php include 'header.php' ?>
+
+        <section id="cart_items">
+        <div class="container">
+            <div class="breadcrumbs">
+                <ol class="breadcrumb">
+                  <li><a href="index.php">Главная</a></li>
+                  <li class="active">Корзина</li>
+                </ol>
+            </div>
+            <div class="table-responsive cart_info">
+                <table class="table table-condensed">
+                    <thead>
+                        <tr class="cart_menu">
+                            <td class="image">Товар</td>
+                            <td class="description"></td>
+                            <td class="price">Цена</td>
+                            <td class="quantity">Количество</td>
+                            <td class="total">Итог</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="cart_product">
+                                <a href="product_detailis.html"><img src="logo-apple.jpg" alt=""></a>
+                            </td>
+                            <td class="cart_description">
+                                <h4><a href="product_detailis.html">Apple</a></h4>
+                                <p>Код товара: 1089772</p>
+                            </td>
+                            <td class="cart_price">
+                                <p>99$</p>
+                            </td>
+                            <td class="cart_quantity">
+                                <div class="cart_quantity_button">
+                                    <a class="cart_quantity_up" href=""> + </a>
+                                    <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+                                    <a class="cart_quantity_down" href=""> - </a>
+                                </div>
+                            </td>
+                            <td class="cart_total">
+                                <p class="cart_total_price">99$</p>
+                            </td>
+                            <td class="cart_delete">
+                                <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td class="cart_product">
+                                <a href="product_detailis.html"><img src="logo-apple.jpg" alt=""></a>
+                            </td>
+                            <td class="cart_description">
+                                <h4><a href="product_detailis.html">Apple</a></h4>
+                                <p>Код товара: 1089772</p>
+                            </td>
+                            <td class="cart_price">
+                                <p>99$</p>
+                            </td>
+                            <td class="cart_quantity">
+                                <div class="cart_quantity_button">
+                                    <a class="cart_quantity_up" href=""> + </a>
+                                    <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+                                    <a class="cart_quantity_down" href=""> - </a>
+                                </div>
+                            </td>
+                            <td class="cart_total">
+                                <p class="cart_total_price">99$</p>
+                            </td>
+                            <td class="cart_delete">
+                                <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="cart_product">
+                                <a href="product_detailis.html"><img src="logo-apple.jpg" alt=""></a>
+                            </td>
+                            <td class="cart_description">
+                                <h4><a href="product_detailis.html">Apple</a></h4>
+                                <p>Код товара: 1089772</p>
+                            </td>
+                            <td class="cart_price">
+                                <p>99$</p>
+                            </td>
+                            <td class="cart_quantity">
+                                <div class="cart_quantity_button">
+                                    <a class="cart_quantity_up" href=""> + </a>
+                                    <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+                                    <a class="cart_quantity_down" href=""> - </a>
+                                </div>
+                            </td>
+                            <td class="cart_total">
+                                <p class="cart_total_price">99$</p>
+                            </td>
+                            <td class="cart_delete">
+                                <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section> <!--/#cart_items-->
+<?php include 'footer.php' ?>
