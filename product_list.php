8df10cff3a0a6365f064cc5df4526e7ab82498fe
StanislavAshykhmin <ashykhminstanislav@gmail.com>
2017-12-20 09:54:24 +0200

    create array -> product,price, sum

---
+++ product_list.php
@@ -0,0 +1,14 @@
+<?php 
+
+$product_list = array(
+array('product' => 'iPhone7 16Gb',  'price' => '99', 'sum' => '2'),
+array('product' => 'iPhone7 32Gb',  'price' => '199', 'sum' => '1'),
+array('product' => 'iPhone7 64Gb',  'price' => '299', 'sum' => '0'),
+array('product' => 'iPhone7 Plus 16Gb',  'price' => '199', 'sum' => '3'),
+array('product' => 'iPhone7 Plus 32Gb',  'price' => '299', 'sum' => '2'),
+array('product' => 'iPhone7 Plus 64Gb',  'price' => '399', 'sum' => '5'),
+);
+
+
+
+?>
