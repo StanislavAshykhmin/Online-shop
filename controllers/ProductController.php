9149c9fbcf86c968af7c5eba82fa163edc79e5db
stanislavashykhmin <stanislavashykhmin@example.com>
2018-01-28 19:49:06 +0200

    add getProductById

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,14 +2,21 @@
 
 
 namespace Controllers;
+use Models\Category;
+use Models\Product;
 
 
 class ProductController
 {
 
 
-    public function actionView($id)
+    public function actionView($productId)
     {
+
+        $categories =  array();
+        $categories = Category::getCategoriesList();
+
+        $product = Product::getProductById($productId);
 
         require_once (ROOT . '/views/product/view.php');
 
