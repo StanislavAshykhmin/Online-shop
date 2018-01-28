9ae71693b088cfd82dd6538c900d614547d7adf8
stanislavashykhmin <stanislavashykhmin@example.com>
2018-01-28 18:54:04 +0200

    add product

--- config/routes.php
+++ config/routes.php
@@ -4,5 +4,6 @@
         'user/login' => 'user/login',
         'user/logout' => 'user/logout',
         'cabinet' => 'cabinet/index',
+        'product/([0-9]+)' => 'product/view/$1',
         '' => 'site/index',
     );
