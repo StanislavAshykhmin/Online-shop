2e272eaa7161187ac6500bd6d522fbed4b753b49
stanislavashykhmin <stanislavashykhmin@example.com>
2018-01-28 19:41:00 +0200

    add getProductsListByCategory

--- models/Product.php
+++ models/Product.php
@@ -7,13 +7,13 @@ namespace Models;
 class Product
 {
 
-    const SHOW_BY_DEFAULT  = 10;
+    const SHOW_BY_DEFAULT = 10;
 
     public static function getLatestProducts($count = self::SHOW_BY_DEFAULT)
     {
         $count = intval($count);
 
-        $db  = \Db::getConnection();
+        $db = \Db::getConnection();
 
         $productsList = array();
 
@@ -25,7 +25,7 @@ class Product
         $i = 0;
 
 
-        while  ($row=$result->fetch()){
+        while ($row = $result->fetch()) {
             $productsList[$i]['id'] = $row['id'];
             $productsList[$i]['name'] = $row['name'];
             $productsList[$i]['image'] = $row['image'];
@@ -35,6 +35,33 @@ class Product
         }
 
         return $productsList;
+    }
+
+
+    public static function getProductsListByCategory($categoryId = false)
+    {
+        if ($categoryId) {
+
+            $db = \Db::getConnection();
+            $products = array();
+            $result = $db->query("SELECT id, name, price, image, is_new FROM product "
+                . "WHERE status = '1' AND category_id = '$categoryId' "
+                . "ORDER BY id DESC "
+                . "LIMIT ".self::SHOW_BY_DEFAULT);
+
+
+            $i = 0;
+            while ($row = $result->fetch()) {
+                $products[$i]['id'] = $row['id'];
+                $products[$i]['name'] = $row['name'];
+                $products[$i]['image'] = $row['image'];
+                $products[$i]['price'] = $row['price'];
+                $products[$i]['is_new'] = $row['is_new'];
+                $i++;
+            }
+
+            return $products;
+        }
     }
 
 }
