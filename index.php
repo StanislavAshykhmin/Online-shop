8724b5f07e56db4c0ed70f0f6df74ecb363ffdca
stanislavashykhmin <stanislavashykhmin@example.com>
2018-01-28 14:53:34 +0200

    add db

--- index.php
+++ index.php
@@ -8,11 +8,14 @@
     ini_set('display_errors',1);
     error_reporting(E_ALL);
 
-    phpinfo();
+
     // 2. Подключение файлов системы
     define('ROOT', dirname(__FILE__));
     require_once (ROOT. '/vendor/autoload.php');
+    require_once (ROOT. '/components/Db.php');
     require_once (ROOT. '/components/Router.php');
+
+
     
 
     // 3. Установка соединения с БД
