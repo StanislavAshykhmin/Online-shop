8b676443ab1b414ec7559bc8b873a73a2ae20e61
stanislavashykhmin <stanislavashykhmin@example.com>
2018-01-30 11:55:38 +0200

    page navigation

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,12 @@
 <?php
 
     return array(
-        'user/login' => 'user/login',
-        'user/logout' => 'user/logout',
-        'cabinet' => 'cabinet/index',
+//        'user/login' => 'user/login',
+//        'user/logout' => 'user/logout',
+//        'cabinet' => 'cabinet/index',
         'product/([0-9]+)' => 'product/view/$1',
         'catalog' => 'catalog/index',
+        'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
         'category/([0-9]+)' => 'catalog/category/$1',
         '' => 'site/index',
     );
