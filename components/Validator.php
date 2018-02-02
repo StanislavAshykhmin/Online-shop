d770667c60d3f7b2befe7509369ecfba3ab1c576
stanislavashykhmin <stanislavashykhmin@example.com>
2018-02-02 12:28:18 +0200

    created validator

---
+++ components/Validator.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace components;
+
+
+class Validator
+{
+
+
+    public static function checkName($name) {
+        if (strlen($name) >=2){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static function checkPassword($password) {
+        if (strlen($password)>=8){
+            return true;
+        }
+        return false;
+    }
+
+    public static function checkEmail($email) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return true;
+        }
+        return false;
+    }
+
+    
+}
