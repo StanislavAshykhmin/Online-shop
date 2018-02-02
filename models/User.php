e3525dac1f3bb8eec716d75720fd13e988bf32cd
stanislavashykhmin <stanislavashykhmin@example.com>
2018-02-02 12:27:41 +0200

    add register

--- models/User.php
+++ models/User.php
@@ -4,20 +4,32 @@ namespace Models;
 class User
 {
 
+    public static function register($name, $email, $password){
+        $db = \Db::getConnection();
+        $sql = 'INSERT INTO user (name, email, password) '
+                . 'VALUES (:name, :email, :password)';
+        $result = $db->prepare($sql);
+        $result->bindParam(':name', $name, \PDO::PARAM_STR);
+        $result->bindParam(':email', $email, \PDO::PARAM_STR);
+        $result->bindParam(':password', $password, \PDO::PARAM_STR);
 
-    public static function checkPassword($password) {
-        if (strlen($password) >=4){
-            return true;
-        }
-        return false;
+        return $result->execute();
     }
 
-    public static function checkEmail($email) {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+    public static function checkEmailExists($email){
+        $db = \Db::getConnection();
+
+        $sql = 'SELECT COUNT(*) FROM user WHERE email = :email';
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':email', $email, \PDO::PARAM_STR);
+        $result->execute();
+
+        if ($result->fetchColumn())
             return true;
-        }
         return false;
     }
+    
 
     public static function checkUserData($email, $password) {
         $em = 'test@test.com';
