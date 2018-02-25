f6af21d687e8b1f9fb41f2cbe201a699e7eea13e
stanislavashykhmin <stanislavashykhmin@example.com>
2018-02-25 22:32:36 +0200

    add new routes

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,13 @@
 <?php
 
     return array(
-//        'user/login' => 'user/login',
-//        'user/logout' => 'user/logout',
-//        'cabinet' => 'cabinet/index',
         'product/([0-9]+)' => 'product/view/$1',
         'catalog' => 'catalog/index',
         'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
         'category/([0-9]+)' => 'catalog/category/$1',
+        'user/register' => 'user/register',
+        'user/logout' => 'user/logout',
+        'user/login' => 'user/login',
+        'cabinet' => 'cabinet/index',
         '' => 'site/index',
     );
