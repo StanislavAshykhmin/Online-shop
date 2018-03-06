61075fe36f5e6af312c7e1496d99967a59b865c7
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-06 17:46:26 +0200

    add recommended product

--- models/Product.php
+++ models/Product.php
@@ -116,5 +116,25 @@ class Product
         return $products;
     }
 
+    public static function getRecommendedProducts(){
+        $db = \Db::getConnection();
+
+        $result = $db->query('SELECT id, name, price, is_new FROM product '
+            . 'WHERE status = "1" AND is_recommended = "1" '
+            . 'ORDER BY id DESC');
+
+
+        $i = 0;
+        $productsList = array();
+        while ($row = $result->fetch()) {
+            $productsList[$i]['id'] = $row['id'];
+            $productsList[$i]['name'] = $row['name'];
+            $productsList[$i]['price'] = $row['price'];
+            $productsList[$i]['is_new'] = $row['is_new'];
+            $i++;
+        }
+        return $productsList;
+    }
+
 
 }
