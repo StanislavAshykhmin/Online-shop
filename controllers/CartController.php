618bc83e38e52403addcd4c61f300e504e121be3
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-06 13:58:56 +0200

    add actionCheckout

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -9,8 +9,11 @@
 namespace controllers;
 
 use Components\Cart;
+use Components\Validator;
 use Models\Category;
 use Models\Product;
+use Models\User;
+use Models\Order;
 
 class CartController
 {
@@ -24,6 +27,7 @@ class CartController
 
 
     }
+    
     public function actionAddAjax($id){
         echo Cart::addProduct($id);
         return true;
@@ -47,4 +51,108 @@ class CartController
         return true;
     }
 
+    public function actionCheckout()
+    {
+
+        // Список категорий для левого меню
+        $categories = array();
+        $categories = Category::getCategoriesList();
+
+
+        // Статус успешного оформления заказа
+        $result = false;
+
+
+        // Форма отправлена?
+        if (isset($_POST['submit'])) {
+            // Форма отправлена? - Да
+            // Считываем данные формы
+            $userName = $_POST['userName'];
+            $userPhone = $_POST['userPhone'];
+            $userComment = $_POST['userComment'];
+
+            // Валидация полей
+            $errors = false;
+            if (!Validator::checkName($userName))
+                $errors[] = 'Неправильное имя';
+            if (!Validator::checkPhone($userPhone))
+                $errors[] = 'Неправильный телефон';
+
+            // Форма заполнена корректно?
+            if ($errors == false) {
+                // Форма заполнена корректно? - Да
+                // Сохраняем заказ в базе данных
+                // Собираем информацию о заказе
+                $productsInCart = Cart::getProducts();
+                if (User::isGuest()) {
+                    $userId = false;
+                } else {
+                    $userId = User::checkLogged();
+                }
+
+                // Сохраняем заказ в БД
+                $result = Order::save($userName, $userPhone, $userComment, $userId, $productsInCart);
+
+
+                if ($result) {
+                    // Оповещаем администратора о новом заказе
+                    $adminEmail = 'testlocalhost1234@gmail.com';
+                    $message = 'http://online-shop.local/admin/orders';
+                    $subject = 'Новый заказ!';
+                    mail($adminEmail, $subject, $message);
+
+                    // Очищаем корзину
+                    Cart::clear();
+                }
+            } else {
+                // Форма заполнена корректно? - Нет
+                // Итоги: общая стоимость, количество товаров
+                $productsInCart = Cart::getProducts();
+                $productsIds = array_keys($productsInCart);
+                $products = Product::getProductsByIds($productsIds);
+                $totalPrice = Cart::getTotalPrice($products);
+                $totalQuantity = Cart::countItems();
+            }
+        } else {
+            // Форма отправлена? - Нет
+            // Получием данные из корзины
+            $productsInCart = Cart::getProducts();
+
+            // В корзине есть товары?
+            if ($productsInCart == false) {
+                // В корзине есть товары? - Нет
+                // Отправляем пользователя на главную искать товары
+                header("Location: /");
+            } else {
+                // В корзине есть товары? - Да
+                // Итоги: общая стоимость, количество товаров
+                $productsIds = array_keys($productsInCart);
+                $products = Product::getProductsByIds($productsIds);
+                $totalPrice = Cart::getTotalPrice($products);
+                $totalQuantity = Cart::countItems();
+
+
+                $userName = false;
+                $userPhone = false;
+                $userComment = false;
+
+                // Пользователь авторизирован?
+                if (User::isGuest()) {
+                    // Нет
+                    // Значения для формы пустые
+                } else {
+                    // Да, авторизирован
+                    // Получаем информацию о пользователе из БД по id
+                    $userId = User::checkLogged();
+                    $user = User::getUserById($userId);
+                    // Подставляем в форму
+                    $userName = $user['name'];
+                }
+            }
+        }
+
+        require_once(ROOT . '/views/cart/checkout.php');
+
+        return true;
+    }
 }
