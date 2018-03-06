e7f3588bb3418ded5ea0ad1098b4b030a8cb264b
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-06 13:58:01 +0200

    add checkout

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,8 @@
         'catalog' => 'catalog/index',
         'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
         'category/([0-9]+)' => 'catalog/category/$1',
+        'cart/checkout' => 'cart/checkout',
+        
         'cart/add/([0-9]+)' => 'cart/add/$1',
         'cart/addAjax/([0-9]+)' => 'cart/addAjax/$1',
         'cart' => 'cart/index',
