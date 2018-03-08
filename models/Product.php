66a2dcd927f2da38fb17bd1d911a1a714ee8488c
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-08 18:42:42 +0200

    delete getCategoriesByList

--- models/Product.php
+++ models/Product.php
@@ -37,36 +37,7 @@ class Product
         return $productsList;
     }
 
-
-    public static function getProductsListByCategory($categoryId = false, $page = 1)
-    {
-        if ($categoryId) {
-
-            $page  = intval($page);
-            $offset = ($page - 1  ) * self::SHOW_BY_DEFAULT;
-
-            $db = \Db::getConnection();
-            $products = array();
-            $result = $db->query("SELECT id, name, price, image, is_new FROM product "
-                . "WHERE status = '1' AND category_id = '$categoryId' "
-                . "ORDER BY id DESC "
-                . "LIMIT ".self::SHOW_BY_DEFAULT
-                . ' OFFSET '. $offset);
-
-
-            $i = 0;
-            while ($row = $result->fetch()) {
-                $products[$i]['id'] = $row['id'];
-                $products[$i]['name'] = $row['name'];
-                $products[$i]['image'] = $row['image'];
-                $products[$i]['price'] = $row['price'];
-                $products[$i]['is_new'] = $row['is_new'];
-                $i++;
-            }
-
-            return $products;
-        }
-    }
+    
 
     public static function getProductById($id)
     {
