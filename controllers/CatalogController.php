1287cf73a5b3d3b947d4c6afe981d39531fc9949
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-08 18:43:22 +0200

    add sort

--- controllers/CatalogController.php
+++ controllers/CatalogController.php
@@ -5,6 +5,7 @@ namespace Controllers;
 use Models\Category;
 use Models\Product;
 use Components\Pagination;
+use Components\Sort;
 
 
 class CatalogController
@@ -16,8 +17,23 @@ class CatalogController
         $categories = array();
         $categories = Category::getCategoriesList();
 
+
+        $sort ='id DESC';
+
+            if (isset($_POST['id_ASC'])){$sort = 'id ASC';}
+            elseif (isset($_POST['id_DESC'])){$sort = 'id DESC';}
+            elseif (isset($_POST['price_ASC'])){$sort = 'price ASC';}
+            elseif (isset($_POST['price_DESC'])){$sort = 'price DESC';}
+
         $latestProducts = array();
-        $latestProducts = Product::getLatestProducts(12);
+        $latestProducts = Sort::sort($sort);
+        
+        
+        
+        //Нужно сделать ajax!!!!!
+        
+        
+        
 
 
         require_once (ROOT  .  '/views/catalog/index.php');
@@ -25,6 +41,8 @@ class CatalogController
         return true;
 
     }
+
+
 
     public function actionCategory($categoryId, $page = 1)
     {
@@ -34,10 +52,32 @@ class CatalogController
         $categories = array();
         $categories = Category::getCategoriesList();
 
+        $sort ='id DESC';
+
+
+
+        if (isset($_POST['id_ASC'])){unset($sort); $sort = 'id ASC';}
+        elseif (isset($_POST['id_DESC'])){unset($sort);$sort = 'id DESC';}
+        elseif (isset($_POST['price_ASC'])){unset($sort);$sort = 'price ASC';}
+        elseif (isset($_POST['price_DESC'])){unset($sort);$sort = 'price DESC';}
+
+
+
+
+
+        //Сортировка некорректно работает с пагинацией, исправить!!!
+
+
+
+
+
+
+
         $categoryProducts = array();
-        $categoryProducts = Product::getProductsListByCategory($categoryId, $page);
+        $categoryProducts = Sort::sortCategory($categoryId, $page, $sort);
 
         $total = Product::getTotalProductsInCategory($categoryId);
+
 
         // Создаем объект Pagination - постраничная навигация
         $pagination = new Pagination($total, $page, Product::SHOW_BY_DEFAULT, 'page-');
