af0dfa72ff3d11dd46b5344cb1bec17f1abd78f5
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-13 20:47:21 +0200

    fixed bugs

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: stanislav
- * Date: 04.03.18
- * Time: 15:56
- */
+
 
 namespace controllers;
 
@@ -64,35 +59,29 @@ class CartController
     public function actionCheckout()
     {
 
-        // Список категорий для левого меню
+     
         $categories = array();
         $categories = Category::getCategoriesList();
 
 
-        // Статус успешного оформления заказа
         $result = false;
 
 
-        // Форма отправлена?
         if (isset($_POST['submit'])) {
-            // Форма отправлена? - Да
-            // Считываем данные формы
+       
             $userName = $_POST['userName'];
             $userPhone = $_POST['userPhone'];
             $userComment = $_POST['userComment'];
 
-            // Валидация полей
+   
             $errors = false;
             if (!Validator::checkName($userName))
                 $errors[] = 'Неправильное имя';
             if (!Validator::checkPhone($userPhone))
                 $errors[] = 'Неправильный телефон';
 
-            // Форма заполнена корректно?
             if ($errors == false) {
-                // Форма заполнена корректно? - Да
-                // Сохраняем заказ в базе данных
-                // Собираем информацию о заказе
+        
                 $productsInCart = Cart::getProducts();
                 if (User::isGuest()) {
                     $userId = false;
@@ -100,23 +89,21 @@ class CartController
                     $userId = User::checkLogged();
                 }
 
-                // Сохраняем заказ в БД
+       
                 $result = Order::save($userName, $userPhone, $userComment, $userId, $productsInCart);
 
 
                 if ($result) {
-                    // Оповещаем администратора о новом заказе
+                  
                     $adminEmail = 'testlocalhost1234@gmail.com';
                     $message = 'http://online-shop.local/admin/orders';
                     $subject = 'Новый заказ!';
                     mail($adminEmail, $subject, $message);
 
-                    // Очищаем корзину
                     Cart::clear();
                 }
             } else {
-                // Форма заполнена корректно? - Нет
-                // Итоги: общая стоимость, количество товаров
+
                 $productsInCart = Cart::getProducts();
                 $productsIds = array_keys($productsInCart);
                 $products = Product::getProductsByIds($productsIds);
@@ -124,18 +111,14 @@ class CartController
                 $totalQuantity = Cart::countItems();
             }
         } else {
-            // Форма отправлена? - Нет
-            // Получием данные из корзины
             $productsInCart = Cart::getProducts();
 
-            // В корзине есть товары?
+       
             if ($productsInCart == false) {
-                // В корзине есть товары? - Нет
-                // Отправляем пользователя на главную искать товары
+
                 header("Location: /");
             } else {
-                // В корзине есть товары? - Да
-                // Итоги: общая стоимость, количество товаров
+
                 $productsIds = array_keys($productsInCart);
                 $products = Product::getProductsByIds($productsIds);
                 $totalPrice = Cart::getTotalPrice($products);
@@ -146,16 +129,14 @@ class CartController
                 $userPhone = false;
                 $userComment = false;
 
-                // Пользователь авторизирован?
+                
                 if (User::isGuest()) {
-                    // Нет
-                    // Значения для формы пустые
+  
                 } else {
-                    // Да, авторизирован
-                    // Получаем информацию о пользователе из БД по id
+         
                     $userId = User::checkLogged();
                     $user = User::getUserById($userId);
-                    // Подставляем в форму
+             
                     $userName = $user['name'];
                 }
             }
