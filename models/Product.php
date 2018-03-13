64d538000a1a9863ca59496dcd52b1a7402f99a3
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-13 20:18:27 +0200

    add  getProductList

--- models/Product.php
+++ models/Product.php
@@ -17,7 +17,7 @@ class Product
 
         $productsList = array();
 
-        $result = $db->query('SELECT id, name, price, image, is_new FROM product '
+        $result = $db->query('SELECT id, name, price, is_new FROM product '
             . 'WHERE status = "1"'
             . 'ORDER BY id DESC '
             . 'LIMIT ' . $count);
@@ -28,7 +28,6 @@ class Product
         while ($row = $result->fetch()) {
             $productsList[$i]['id'] = $row['id'];
             $productsList[$i]['name'] = $row['name'];
-            $productsList[$i]['image'] = $row['image'];
             $productsList[$i]['price'] = $row['price'];
             $productsList[$i]['is_new'] = $row['is_new'];
             $i++;
@@ -37,7 +36,22 @@ class Product
         return $productsList;
     }
 
-    
+    public static function getProductsList()
+    {
+        $db = \Db::getConnection();
+
+        $result = $db->query('SELECT id, name, price, code FROM product ORDER BY id ASC');
+        $productsList = array();
+        $i = 0;
+        while ($row = $result->fetch()) {
+            $productsList[$i]['id'] = $row['id'];
+            $productsList[$i]['name'] = $row['name'];
+            $productsList[$i]['code'] = $row['code'];
+            $productsList[$i]['price'] = $row['price'];
+            $i++;
+        }
+        return $productsList;
+    }
 
     public static function getProductById($id)
     {
@@ -52,6 +66,7 @@ class Product
             return $result->fetch();
         }
     }
+    
 
     public static function getTotalProductsInCategory($categoryId)
     {
@@ -64,6 +79,7 @@ class Product
 
         return $row['count'];
     }
+    
 
     public static function getProductsByIds($idsArray){
         $products = array();
@@ -108,5 +124,98 @@ class Product
         return $productsList;
     }
 
+    public static function deleteProductsById($id){
+        $db = \Db::getConnection();
 
+        $sql = 'DELETE FROM product WHERE id = :id';
+
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':id', $id, \PDO::PARAM_INT);
+        return $result->execute();
+    }
+
+
+
+    public static function createProduct($options)
+    {
+        $db = \Db::getConnection();
+
+        $sql = 'INSERT INTO product '
+            . '(name, code, price, category_id, brand, availability, '
+            . 'description, is_new, is_recommended, status) '
+            . 'VALUES '
+            . '(:name, :code, :price, :category_id, :brand, :availability, '
+            . ':description, :is_new, :is_recommended, :status)';
+
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':name', $options['name'], \PDO::PARAM_STR);
+        $result->bindParam(':code', $options['code'], \PDO::PARAM_STR);
+        $result->bindParam(':price', $options['price'], \PDO::PARAM_STR);
+        $result->bindParam(':category_id', $options['category_id'], \PDO::PARAM_INT);
+        $result->bindParam(':brand', $options['brand'], \PDO::PARAM_STR);
+        $result->bindParam(':availability', $options['availability'], \PDO::PARAM_INT);
+        $result->bindParam(':description', $options['description'], \PDO::PARAM_STR);
+        $result->bindParam(':is_new', $options['is_new'], \PDO::PARAM_INT);
+        $result->bindParam(':is_recommended', $options['is_recommended'], \PDO::PARAM_INT);
+        $result->bindParam(':status', $options['status'], \PDO::PARAM_INT);
+        if ($result->execute()) {
+            return $db->lastInsertId();
+        }
+        return 0;
+    }
+
+    public static function updateProductById($id, $options)
+    {
+        $db = \Db::getConnection();
+
+        $sql = "UPDATE product
+            SET 
+                name = :name, 
+                code = :code, 
+                price = :price, 
+                category_id = :category_id, 
+                brand = :brand, 
+                availability = :availability, 
+                description = :description, 
+                is_new = :is_new, 
+                is_recommended = :is_recommended, 
+                status = :status
+            WHERE id = :id";
+
+        $result = $db->prepare($sql);
+        $result->bindParam(':id', $id, \PDO::PARAM_INT);
+        $result->bindParam(':name', $options['name'], \PDO::PARAM_STR);
+        $result->bindParam(':code', $options['code'], \PDO::PARAM_STR);
+        $result->bindParam(':price', $options['price'], \PDO::PARAM_STR);
+        $result->bindParam(':category_id', $options['category_id'], \PDO::PARAM_INT);
+        $result->bindParam(':brand', $options['brand'], \PDO::PARAM_STR);
+        $result->bindParam(':availability', $options['availability'], \PDO::PARAM_INT);
+        $result->bindParam(':description', $options['description'], \PDO::PARAM_STR);
+        $result->bindParam(':is_new', $options['is_new'], \PDO::PARAM_INT);
+        $result->bindParam(':is_recommended', $options['is_recommended'], \PDO::PARAM_INT);
+        $result->bindParam(':status', $options['status'], \PDO::PARAM_INT);
+        return $result->execute();
+    }
+    public static function getImage($id)
+    {
+        // Название изображения-пустышки
+        $noImage = 'no-image.jpg';
+
+        // Путь к папке с товарами
+        $path = '/upload/images/products/';
+
+        // Путь к изображению товара
+        $pathToProductImage = $path . $id . '.jpg';
+
+        if (file_exists($_SERVER['DOCUMENT_ROOT'].$pathToProductImage)) {
+            // Если изображение для товара существует
+            // Возвращаем путь изображения товара
+            return $pathToProductImage;
+        }
+
+        // Возвращаем путь изображения-пустышки
+        return $path . $noImage;
+    }
 }
