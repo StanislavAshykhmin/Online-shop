b309784134fabbe48bfe4096d573d4033bab7d2e
stanislavashykhmin <stanislavashykhmin@example.com>
2018-03-14 05:38:56 +0200

    update

--- models/User.php
+++ models/User.php
@@ -4,16 +4,21 @@ namespace Models;
 class User
 {
 
-    public static function register($name, $email, $password){
+    public static function register($name, $email, $password, $role)
+    {
         $db = \Db::getConnection();
-        $sql = 'INSERT INTO user (name, email, password) '
-                . 'VALUES (:name, :email, :password)';
+
+
+        $sql = 'INSERT INTO user (name, email, password, role) '
+            . 'VALUES (:name, :email, :password, :role)';
+
         $result = $db->prepare($sql);
         $result->bindParam(':name', $name, \PDO::PARAM_STR);
         $result->bindParam(':email', $email, \PDO::PARAM_STR);
         $result->bindParam(':password', $password, \PDO::PARAM_STR);
-
+        $result->bindParam(':role', $role, \PDO::PARAM_STR);
         return $result->execute();
+        
     }
 
     public static function edit($id, $name, $password)
@@ -92,6 +97,15 @@ class User
             return $_SESSION['user'];
         }
         header("Location: /user/login");
+    }
+
+    public static function checkLoggedText()
+    {
+        //Если сессия есть, вернем индетификатор пользователя
+        if (isset($_SESSION['user'])) {
+            return $_SESSION['user'];
+        }
+        return false;
     }
 
     public static function isGuest(){
